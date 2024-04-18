46cb3be7a4b988f9d35abbb534cba2928a554c84
puzz-h <xiaoxiaozhu5@126.com>
2024-04-18 09:47:34 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ use quick_xml::se::to_string;
 
 use structopt::StructOpt;
 
-use serde::{Serialize, Deserialize};
+mod field;
+use crate::field::{File, MagicFolder, Project, Workspace};
 
 struct Content {
     path: String,
@@ -104,60 +105,6 @@ impl Consume for MyConsumer {
 
     fn err(&self, path: &str) {
         println!("Error parsing file: {}", path);
-    }
-}
-
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct File {
-    #[serde(rename = "@path")]
-    pub Path: String,
-}
-
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct MagicFolder {
-    #[serde(rename = "@excludeFolders")]
-    pub exclude: String,
-    #[serde(rename = "@filter")]
-    pub filter: String,
-    #[serde(rename = "@name")]
-    pub name: String,
-    #[serde(rename = "@path")]
-    pub path: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub File: Option<Vec<File>>,
-}
-
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct Project {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "@path")]
-    pub Path: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(rename = "@name")]
-    pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub File: Option<Vec<File>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub MagicFolder: Option<Vec<MagicFolder>>,
-}
-
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct Workspace {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub File: Option<Vec<File>>,
-    pub Project: Vec<Project>
-}
-
-impl Workspace {
-    pub fn new(projects: Vec<Project>, files: Option<Vec<File>>) -> Self {
-        Workspace {
-            File: files,
-            Project: projects,
-        }
     }
 }
 
